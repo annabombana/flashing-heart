017d9b89dae111043e63b76bdbcce7726dbcf083
annabombana <99048726+annabombana@users.noreply.github.com>
2022-02-04 20:53:35 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
